9ccac967ab7e32739f6043cfd35926fe6e9d2049
Suryachand <61744673+psschand16@users.noreply.github.com>
2023-01-18 10:03:04 +0000

    Update counter.js

--- src/js/components/counter.js
+++ src/js/components/counter.js
@@ -2,6 +2,9 @@
 class Counter extends HTMLElement {
     constructor() {
         super();
+        console.log(import.meta.env.VITE_SOME_KEY) // 123
+        console.log(import.meta.env.DB_PASSWORD) // undefined
+
         
         this.innerHTML = /*html*/`
             <button>Clicks : ${count}</button>
